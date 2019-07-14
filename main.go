3080d8975a4f5f7be1987ef07c6e95537934a61d
sisimogangg <godfrey@sisimogang.co.za>
2019-07-14 19:52:15 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func gen() <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer func() {
+			close(out)
+		}()
+
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+
+	}()
+
+	return out
+}
+
+func sumUp(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer func() {
+			close(out)
+		}()
+
+		sum := 0
+		for i := range in {
+			sum += i
+		}
+		out <- sum
+
+	}()
+	return out
+}
+
+func main() {
+	//done := make(chan struct{})
+
+	out := gen()
+	sum := sumUp(out)
+
+	fmt.Println(<-sum)
+
+	/*defer func() {
+		if err := recover(); err != nil {
+			log.Println("Shit", err)
+		}
+	}()*/
+
+	time.Sleep(4 * time.Second)
+}
